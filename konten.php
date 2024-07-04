01a2ff7e13ede27062258a1a5cc42ac6c262c69c
KiranaChandra <chandnana12@gmail.com>
2024-07-04 21:41:32 +0700

    Update konten.php
    Menambah koneksi dengan database, dapat melihat list konten yg telah diinput

--- konten.php
+++ konten.php
@@ -1,6 +1,37 @@
 <?php
-if(isset($_POST['add'])) {
+if (isset($_POST['add'])) {
     //cek apakah ada gambar
+    if (!empty($_FILES['gambar']['name']) && ($_FILES['gambar']['error'] !== 4)) {
+        $gambarfile = $_FILES['gambar']['tmp_name'];
+        $gambarfile_name = $_FILES['gambar']['name'];
+        $filetype = $_FILES['gambar']['type'];
+        $allowtype = array('image/jpeg', 'image/jpg', 'image/png');
+
+        if (!in_array($filetype, $allowtype)) {
+            echo 'Invalid file type';
+            exit;
+        }
+
+        $path = PATH_GAMBAR . '/';
+        if ($gambarfile && $gambarfile_name) {
+            $gambarbaru = preg_replace("/[^a-zA-Z0-9]/", "_", $_POST['judul']);
+            $dest = $path . $gambarbaru . '.jpg';
+            if (move_uploaded_file($_FILES['gambar']['tmp_name'], $dest)) {
+                $gambar = $dest;
+            } else {
+                echo 'Failed to upload image';
+                exit;
+            }
+        }
+    } else {
+        $gambar = ''; // Set default value if no image uploaded
+    }
+
+    global $connect;
+    $sql = "INSERT INTO berita (Judul, Isi, Kategori, Gambar, Teks, Tanggal, Viewnum, Updateby, Post_type, Terbit)
+            VALUES ('" . $_POST['judul'] . "', '" . $_POST['isi'] . "', '" . $_POST['kategori'] . "', '" . $gambar . "', '"
+             . $_POST['teks'] . "', '" . date("Y-m-d H:i:s") . "', '0', '" . $_SESSION['loginadmin'] . "', 'berita', '" . $_POST['terbit'] . "')";
+    $hasil = mysqli_query($connect, $sql);
 }
 ?>
 
@@ -11,8 +42,7 @@ if(isset($_POST['add'])) {
 
             <div class="formNama">
                 <label>Judul</label>:<br>
-                <input type="text" name="judul" placeholder="Judul Konten" value=""
-                size="40">
+                <input type="text" name="judul" placeholder="Judul Konten" value="" size="40">
             </div>
 
             <div class="formNama">
@@ -21,13 +51,9 @@ if(isset($_POST['add'])) {
                     <option>Pilih Kategori</option>
                     <?php 
                     global $connect;
-                    $hasil = mysqli_query($connect, "SELECT * FROM kategori WHERE
-                             Terbit='1' ORDER BY ID DESC");
-                    while($k = mysqli_fetch_array($hasil)){
-                        echo '
-                        <option value="' . $k ['alias'] .'">' . 
-                        $k['Kategori'] . ' </option>
-                        ';
+                    $hasil = mysqli_query($connect, "SELECT * FROM kategori WHERE Terbit='1' ORDER BY ID DESC");
+                    while ($k = mysqli_fetch_array($hasil)) {
+                        echo '<option value="' . $k['alias'] . '">' . $k['Kategori'] . '</option>';
                     }
                     ?>
                 </select>
@@ -56,8 +82,46 @@ if(isset($_POST['add'])) {
                 </select>
             </div>
             
-            <input type="submit" name="add" value="Tambah">
+            <input type="submit" name="add" value="Tambah" class="btn-primary">
 
         </fieldset>
     </form>
+</div>
+<div class="clear"></div>
+
+<div class="w100">
+    <fieldset>
+        <legend>List Berita</legend>
+        <div class="list fl ">
+            <div class="w10 fl">ID</div>
+            <div class="w40 fl">Judul</div>
+            <div class="w20 fl">Kategori</div>
+            <div class="w20 fl">Tanggal</div>
+            <div class="w10 fl">Aksi</div>
+
+        </div>
+        <?php
+        global $connect;
+
+        $hasil = mysqli_query($connect,"SELECT * FROM berita ORDER BY ID DESC");
+        while($b = mysqli_query($hasil)){
+            extract($b);
+            ?>
+            <div class="w100 list fl">
+            <div class="w10 fl"><?=$ID;?></div>
+            <div class="w40 fl"><?=$Judul;?></div>
+            <div class="w20 fl"><?=$Kategori;?></div>
+            <div class="w20 fl"><?=$Tanggal;?></div>
+            <div class="w10 fl">
+                <a href="#" class="btn-primary">edit</a>
+                <a href="#" class="btn-red pd5">hapus</a>   
+
+            </div>
+        </div>
+
+        <?php
+        }
+        ?>
+        
+    </fieldset>
 </div>
