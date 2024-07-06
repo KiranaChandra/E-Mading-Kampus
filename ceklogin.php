93c752cd85698321201cbaef50a1584d3b7d66b9
Gilang Aditya <gilangaditiya041@gmail.com>
2024-07-06 15:00:09 +0700

    login-page
    menambahkan halaman login untuk user dan admin

---
+++ ceklogin.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+include("../koneksi.php");
+
+if (isset($_POST['submit'])) {
+    $username = mysqli_real_escape_string($connect, $_POST['username']);
+    $password = mysqli_real_escape_string($connect, $_POST['password']);
+    
+    $sql = "SELECT * FROM administrator WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($connect, $sql);
+    
+    if ($result) {
+        $numrow = mysqli_num_rows($result);
+        $r = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        
+        if ($numrow > 0) {
+            $_SESSION['loginadmin'] = $r['username'];
+            $_SESSION['loginadminid'] = $r['ID'];
+            $_SESSION['loginadminemail'] = $r['email'];
+            $_SESSION['loginadminnama'] = $r['Nama'];
+            // Redirect to a protected page if login is successful
+            header('Location: index.php');
+            exit;
+        } else {
+            $eror = "User dan Password tidak cocok";
+            header('Location:index.php?eror=' . urlencode($eror));
+            exit;
+        }
+    } else {
+        die("Query failed: " . mysqli_error($connect));
+    }
+}
+
+if (empty($_SESSION['loginadmin'])) {
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="logininpage">
+    <div class="logo">
+        <!-- Tempat logo jika diperlukan -->
+    </div>
+
+    <form action="" method="POST">
+        <label>Username</label>
+        <input type="text" name="username" placeholder="Username" required>
+
+        <label>Password</label>
+        <input type="password" name="password" required>
+
+        <input type="submit" name="submit" value="Login">
+    </form>
+</div>
+</body>
+</html>
+<?php
+} else {
+    echo "Anda sudah login.";
+}
+//exit;
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include("ceklogin.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Administrator</title>
+</head>
+<body>
+    <h1>ini adalah halaman index.php</h1>
+</body>
+</html>
