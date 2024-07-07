94ba66ad1c4e95a706bf12c880a676e44c71dc08
KiranaChandra <chandnana12@gmail.com>
2024-07-07 15:21:12 +0700

    Menyesuaikan letak file sesuai folder

---
+++ inc/koneksi.php
@@ -0,0 +1,37 @@
+<<<<<<< HEAD:koneksi.php
+<?php
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBNAME', 'emk');
+
+define('URL_SITUS', 'http://localhost/E-Mading_Kampus/');
+define('PATH_LOGO', 'image');
+define('FILE_LOGO', 'logo.png');
+define('FILE_ICON', 'icon.png');
+
+
+$connect = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+
+if (mysqli_connect_error()) {
+    echo "Gagal Koneksi ke Database: " . mysqli_connect_error();
+
+}
+?>
+=======
+<?php
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBNAME', 'emk');
+
+define('URL_SITUS', 'http://localhost/E-Mading_Kampus/');
+
+$connect = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+
+if (mysqli_connect_error()) {
+    echo "Gagal Koneksi ke Database: " . mysqli_connect_error();
+
+}
+?>
+>>>>>>> 3df05400676220b4c22dcef28a4441f785faaeef:admin/koneksi.php
