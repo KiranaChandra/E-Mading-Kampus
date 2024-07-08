255a3cdc46f66b7205b173bc1e5771cd36861f24
upianandsyah30 <achmadluthfi312@gmail.com>
2024-07-08 18:02:31 +0700

    menambahkan-halaman-konfigurasi

--- admin/ceklogin.php
+++ admin/ceklogin.php
@@ -1,80 +1,3 @@
-<<<<<<< HEAD:ceklogin.php
-<?php
-session_start();
-
-if  ($_GET['keluar']=='yes'){
-    session_destroy();
-    header("Location:index.php");
-}
-include("../koneksi.php");
-
-if (isset($_POST['submit'])) {
-    $username = mysqli_real_escape_string($connect, $_POST['username']);
-    $password = mysqli_real_escape_string($connect, $_POST['password']);
-    
-    $sql = "SELECT * FROM administrator WHERE username='$username' AND password='$password'";
-    $result = mysqli_query($connect, $sql);
-    
-    if ($result) {
-        $numrow = mysqli_num_rows($result);
-        $r = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        
-        if ($numrow > 0) {
-            $_SESSION['loginadmin'] = $r['username'];
-            $_SESSION['loginadminid'] = $r['ID'];
-            $_SESSION['loginadminemail'] = $r['email'];
-            $_SESSION['loginadminnama'] = $r['Nama'];
-            // Redirect to a protected page if login is successful
-            header('Location: index.php');
-            exit;
-        } else {
-            $eror = "User dan Password tidak cocok";
-            header('Location:index.php?eror=' . urlencode($eror));
-            exit;
-        }
-    } else {
-        die("Query failed: " . mysqli_error($connect));
-    }
-}
-
-if (empty($_SESSION['loginadmin'])) {
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-<div class="w20 fn loginpage">
-    <div class="logo">
-        <!-- Tempat logo jika diperlukan -->
-    </div>
-
-    <form action="" method="POST">
-        <div class="user">
-            <label>Username</label><br>
-            <input type="text" name="username" placeholder="Username" required>
-        </div>
-
-        <div class="user">
-            <label>Password</label><br>
-            <input type="password" name="password" required>
-        </div>
-        
-        <input type="submit" name="submit" value="Login">
-    </form>
-</div>
-</body>
-</html>
-<?php
-} else {
-    echo "Anda sudah login.";
-}
-//exit;
-?>
-=======
 <?php
 session_start();
 include("koneksi.php");
@@ -120,8 +43,10 @@ if (empty($_SESSION['loginadmin'])) {
 <body>
 <div class="logininpage">
     <div class="logo">
+        <img src= "<?=URL_SITUS.PATH_LOGO.'/'.FILE_LOGO;?>"> 
         <!-- Tempat logo jika diperlukan -->
     </div>
+    <div class="clear"></DIV>
 
     <form action="" method="POST">
         <label>Username</label>
@@ -141,4 +66,3 @@ if (empty($_SESSION['loginadmin'])) {
 }
 //exit;
 ?>
->>>>>>> 3df05400676220b4c22dcef28a4441f785faaeef:admin/ceklogin.php
--- admin/konfigurasi.php
+++ admin/konfigurasi.php
@@ -1,72 +1,138 @@
 <?php
+if ((isset($_POST['tambahkonfigurasi']))) {
+    global $connect;
+    $sql = mysqli_query($connect, "INSERT INTO konfigurasi (Nama, Tax, Isi, Link, Tipe) 
+    VALUES ('" . $_POST['Nama'] . "','" . $_POST['Tax'] . "','" . $_POST['Isi'] . "','" . $_POST['Link'] . "','konfigurasi')");
+}
+if (isset($_POST['editkonfigurasi'])){
+    $count = 0; 
+    foreach($_POST['Nama'] as $item){
+        $sql = "UPDATE konfigurasi SET Nama ='"$_POST['Nama'][$count]"',Tax = '"$_POST['Tax'][$count]"',Isi ='"$_POST['Isi'][$count]"',Link = '"$_POST['Link'][$count]"'
+        WHERE ID='"$_POST['ID'][$count]"' " ;
+        $hasil  = mysqli_query($connect,$sql);
+        $count++;
+
+    }
+}
+if (isset($_GET['act'] && ($_GET['act'] == 'hapus' ))){
+    $id = (int)$_GET['id'];
+    $hasil = mysqli_query($connect,"DELETE FROM konfigurasi WHERE ID ='$id '");
+}
 if (isset($_POST['uploadlogo'])) {
-     if (!empty($_FILES['logositus']['name']) && ($_FILES['logositus']['eror'] !== 4))
-     {
-        $filetype = $_FILE['logositus']['type'];
-        $allowtype = array ('image/jpeg', 'image/jpg', 'image/png');
+    if (!empty($_FILES['logositus']['name']) && ($_FILES['logositus']['error'] !== 4)) {
+        $filetype = $_FILES['logositus']['type'];
+        $allowtype = array('image/jpeg', 'image/jpg', 'image/png');
 
-        if(!in_array($filetype, $allowtype))
-        {   
+        if (!in_array($filetype, $allowtype)) {
             echo 'Invalid file type';
-        }else {
-            $dest = '../'.PATH_LOGO'/'.FILE_LOGO;
-
+        } else {
+            $dest = '../' . PATH_LOGO . '/' . FILE_LOGO;
             copy($_FILES['logositus']['tmp_name'], $dest);
-
         }
-     }
+    }
 }
 
 if (isset($_POST['uploadicon'])) {
-    if (!empty($_FILES['iconsitus']['name']) && ($_FILES['iconsitus']['eror'] !== 4))
-    {
-       $filetype = $_FILE['iconsitus']['type'];
-       $allowtype = array ('image/png', 'image/gif');
+    if (!empty($_FILES['iconsitus']['name']) && ($_FILES['iconsitus']['error'] !== 4)) {
+        $filetype = $_FILES['iconsitus']['type'];
+        $allowtype = array('image/png', 'image/gif');
 
-       if(!in_array($filetype, $allowtype))
-       {   
-           echo 'Invalid file type';
-       }else {
-           $dest = '../'.FILE_ICON;
-
-           copy($_FILES['logositus']['tmp_name'], $dest);
-
-       }
+        if (!in_array($filetype, $allowtype)) {
+            echo 'Invalid file type';
+        } else {
+            $dest = '../' . FILE_ICON;
+            copy($_FILES['iconsitus']['tmp_name'], $dest);
+        }
     }
 }
 ?>
 <div class="w60 f1">
-<form action="./?mod=konfigurasi" method="POST" enctype="multipart/form-data">
-    
-        <legend>Logo Situs</legend>
-
-        <img src="?=URL_SITUS.PATH_LOGO.'/'.FILE_LOGO;?>" width="250">
-
-        <div class="clear">
-
-        <input type="file" nama="logositus">
-
-         <input type="file" nama="uploadlogo" value="Upload Logo">
-    </fiedset>
-</form>
+    <form action="./?mod=konfigurasi" method="POST" enctype="multipart/form-data">
+        <fieldset>
+            <legend>Logo Situs</legend>
+            <img src="<?= URL_SITUS . PATH_LOGO . '/' . FILE_LOGO; ?>" width="250">
+            <div class="clear"></div>
+            <input type="file" name="logositus">
+            <input type="submit" name="uploadlogo" value="Upload Logo">
+        </fieldset>
+    </form>
 </div>
 
 <div class="w40 f1">
-<form action="./?mod=konfigurasi" method="POST" enctype="multipart/form-data">
-    <fieldset>
-        <legend>Icon Situs</legend>
-
-        <img src="?=URL_SITUS.'/'.FILE_ICON;?>" width="50">
-
-        <div class="clear">
-
-        <input type="file" nama="iconsitus">
-
-         <input type="file" nama="uploadicon" value="Upload Icon">
-    </fiedset>
-</form>
+    <form action="./?mod=konfigurasi" method="POST" enctype="multipart/form-data">
+        <fieldset>
+            <legend>Icon Situs</legend>
+            <img src="<?= URL_SITUS . '/' . FILE_ICON; ?>" width="50">
+            <div class="clear"></div>
+            <input type="file" name="iconsitus">
+            <input type="submit" name="uploadicon" value="Upload Icon">
+        </fieldset>
+    </form>
 </div>
 
-<div class="clear">
+<div class="clear"></div>
 
+<div class="w40 f1">
+    <form action="./?mod=konfigurasi" method="POST">
+        <fieldset>
+            <legend>Tambah Konfigurasi</legend>
+            <div class="w20 fl pd5 bg_dark bold">Nama</div>
+            <div class="w20 fl pd5 bg_dark bold">Tax</div>
+            <div class="w20 fl pd5 bg_dark bold">Isi</div>
+            <div class="w20 fl pd5 bg_dark bold">Link</div>
+            <div class="w20 fl pd5 bg_grey">
+                <input type="text" name="Nama" placeholder="Nama" class="form100">
+            </div>
+            <div class="w15 fl pd5 bg_grey">
+                <input type="text" name="Tax" placeholder="Tax" class="form100">
+            </div>
+            <div class="w30 fl pd5 bg_grey">
+                <input type="text" name="Isi" placeholder="Isi" class="form100">
+            </div>
+            <div class="w30 fl pd5 bg_grey">
+                <input type="text" name="Link" placeholder="Link" class="form100">
+            </div>
+            <div class="clear pd5"></div>
+            <input type="submit" name="tambahkonfigurasi" value="TAMBAH" class="btn-primary">
+            <div class="clear"></div>
+        </fieldset>
+    </form>
 </div>
+
+<div class="clear"></div>
+
+<div class="w40 f1">
+    <form action="./?mod=konfigurasi" method="POST">
+        <fieldset>
+            <legend>List Konfigurasi</legend>
+            <?php
+            global $connect;
+            $hasil = mysqli_query($connect, "SELECT * FROM konfigurasi WHERE Tipe = 'konfigurasi'");
+            while ($r = mysqli_fetch_array($hasil)) {
+                extract($r);
+            ?>
+            <intput type="hidden" name= "id[]" value = " <?=$ID;?>"></intput>
+            <div class="w20 fl pd5 bg_grey">
+                <input type="text" name="Nama[]" value="<?= $Nama ?>" class="form100">
+            </div>
+            <div class="w15 fl pd5 bg_grey">
+                <input type="text" name="Tax[]" value="<?= $Tax ?>" class="form100">
+            </div>
+            <div class="w30 fl pd5 bg_grey">
+                <input type="text" name="Isi[]" value="<?= $Isi ?>" class="form100">
+            </div>
+            <div class="w30 fl pd5 bg_grey">
+                <input type="text" name="Link[]" value="<?= $Link ?>" class="w90"><a href= "./mod=konfigurasi&act=hapus&id=<?=$ID;?>">
+                <span class = "pd5 m15 bg_dark center">x </span></a>
+            </div>
+            <?php
+            }
+            ?>
+            <div class="clear pd5"></div>
+            <input type="submit" name="editkonfigurasi" value="EDIT" class="btn-primary">
+            <div class="clear"></div>
+        </fieldset>
+    </form>
+</div>
+<div class="clear"></div>
+
